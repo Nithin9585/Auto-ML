6282f8b26e2f7f8c24da484cfb77dd15480525f1
Nithin <nithin958595@gmail.com>
2024-10-14 00:59:58 +0530

    user auth session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+"use client";
+import React, { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import Footer from '@/components/Footer';
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/lib/firebase';
+import { useRouter } from 'next/navigation';
 export default function Home() {
+  const [user, loading] = useAuthState(auth); 
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push('/Sign-up');
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <div>Loading...</div>; 
+  }
+
   return (
-    <div className='flex-row w-full min-h-screen justify-center items-center'>
+    <div className='flex flex-col items-center justify-center w-full min-h-screen'>
       <Link href="/Uploadcsv">
         <Button>
           Get Started...
         </Button>
-        </Link>
-
-       
-      <Footer/>
+      </Link>
+      <Footer />
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link';
 import { Button } from './ui/button';
 import {
@@ -9,65 +10,101 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { ModeToggle } from './ui/theme-btn';
+import { auth } from '@/lib/firebase';
+import { signOut } from 'firebase/auth'; 
+import { useRouter } from 'next/navigation';
 
 export default function Navbar() {
+  const router = useRouter(); 
+  const userSession = sessionStorage.getItem('user');
+
+  if (!userSession) {
+    router.push('/Sign-up');
+  }
+
   return (
-    <nav className="bg-background/50 sticky top-0 backdrop-blur   border-b p-4 z-50">      <div className="container mx-auto flex justify-between items-center">
-        <div className=" text-2xl text-purple-600 font-bold">
-          <Link  href="/">
-            Automl
+    <nav className="bg-background/50 flex-row sticky top-0 backdrop-blur border-b p-4 z-50">
+     
+
+      <div className="container mx-auto justify-between  flex items-center">
+      <div className="text-2xl  text-purple-600 font-bold">
+          <Link href="/">Automl</Link>
+          </div>
+        <div className="hidden md:flex   space-x-4 items-center">
+          <Link href="/" className="transition duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">
+            Home
           </Link>
+          <Link href="/blog" className="transition duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">
+            Blog
+          </Link>
+          <Link href="/about" className="transition duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">
+            About
+          </Link>
+          <Link href="/Contact" className="transition duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">
+            Contact
+          </Link>
+          {!userSession && (
+            <div>
+              <Link href="/Login">
+                <Button className="mx-1">Login</Button>
+              </Link>
+              <Link href="/Sign-up">
+                <Button className="mx-1">Sign up</Button>
+              </Link>
+            </div>
+          )}
+          {userSession && (
+            <Button className="mx-1" onClick={() => signOut(auth)}>Logout</Button>
+          )}
         </div>
-      <div className="hidden md:flex space-x-4 items-center">
-  <Link href="/" className="transition   duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-grpurpleeen-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">Home</Link>
-  <Link href="/blog" className="transition  duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">Blog</Link>
-  <Link href="/about" className="transition  duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">About</Link>
-  <Link href="/Contact" className="transition  duration-300 ease-in-out relative inline-block after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-gradient-to-r after:from-purple-900 after:to-purple-200 after:scale-x-0 after:origin-left after:transition-transform after:duration-500 hover:after:scale-x-100 hover:-translate-y-1">Contact</Link>
-
-<div><Link href="/Login">
-    <Button className="mx-1 ">Login</Button>
-    </Link>
-    <Link href="/Sign-up">
-    <Button className="mx-1 ">Sign up</Button>
-    </Link>
-  </div>
-  <ModeToggle />
-</div>
-        <div className="md:hidden">
-          <Sheet>
-            <SheetTrigger>
-            <svg className="w-6 h-6" fill="green" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round"  strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <svg
+              className="w-6 h-6"
+              fill="green"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
-            </SheetTrigger>
-
-            <SheetContent >
-              <SheetHeader>
-                <SheetTitle className='text-purple-600'>Automl</SheetTitle>
-                <SheetDescription>
-                  <div className="flex pt-10 flex-col gap-8 items-center">
-                    <Link href="/" className=" hover:text-purple-300">Home</Link>
-                    <Link href="/blog" className=" hover:text-purple-300">XYZ</Link>
-                    <Link href="/about" className=" hover:text-purple-300">About</Link>
-                    <Link href="/Contact" className=" hover:text-purple-300">Contact</Link>
-                    <div className="flex justify-center absolute bottom-0 left-0 w-full p-4">
-                    <Link href="/Login">
-                      <Button className="mx-3 border-purple-600 hover:bg-purple-400 text-purple-100" variant="outline">Login</Button>
-                      </Link>
-                      <Link href="/Sign-up">
-                      <Button className="mx-3 border-purple-600 hover:bg-purple-400 text-purple-400" variant="outline">Sign up</Button>
-                      </Link>
-                    </div>
-                  </div>
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+          </SheetTrigger>
           <span className='mx-2'>
-            <ModeToggle />
-          </span>
-        </div>
+          <ModeToggle />
+        </span>
+
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="text-purple-600">Automl</SheetTitle>
+              <SheetDescription>
+                <div className="flex pt-10 flex-col gap-8 items-center">
+                  <Link href="/" className="hover:text-purple-300">Home</Link>
+                  <Link href="/blog" className="hover:text-purple-300">Blog</Link>
+                  <Link href="/about" className="hover:text-purple-300">About</Link>
+                  <Link href="/Contact" className="hover:text-purple-300">Contact</Link>
+                  <div className="flex justify-center absolute bottom-0 left-0 w-full p-4">
+                    {!userSession && (
+                      <>
+                        <Link href="/Login">
+                          <Button className="mx-3 border-purple-600 hover:bg-purple-400 text-purple-100" variant="outline">Login</Button>
+                        </Link>
+                        <Link href="/Sign-up">
+                          <Button className="mx-3 border-purple-600 hover:bg-purple-400 text-purple-400" variant="outline">Sign up</Button>
+                        </Link>
+                      </>
+                    )}
+                  </div>
+                </div>
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+        
       </div>
+      
+      </div>
+
     </nav>
   );
 }
